adc9f52abb2b88304056d93aff13cbc1c8274ea5
SonmezYigithan <sonmez.yigithan@gmail.com>
2024-03-08 17:47:59 +0300

    Initial Commit

---
+++ AIChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIChatApp
+//
+//  Created by Yiğithan Sönmez on 8.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
